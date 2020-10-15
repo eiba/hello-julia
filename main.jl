db0d502b3868a9b35990f5e364be0c96d3aaf744
Eirik Baug <eirikbaug@hotmail.com>
2020-10-15 22:40:04 +0200

    fitness function

--- main.jl
+++ main.jl
@@ -2,21 +2,34 @@ function string_as_integer_list(string)
     return [Int(char) for char in string]
 end
 
-function ranom_population_of_length(population_size, length, min_char, max_char)
-    return [
-        random_specimen_of_length(length, min_char, max_char) for i = 1:population_size
-    ]
+function random_population_of_length(population_size, length, min_char, max_char)
+    return [random_specimen_of_length(length, min_char, max_char) for i = 1:population_size]
 end
 
 function random_specimen_of_length(length, min_char, max_char)
     return [rand(min_char:max_char) for i = 1:length]
 end
 
+function fitness(specimen,target)
+    for i = 1:length(specimen)
+        distance = specimen[i] - target[i]
+    end
+end
+
+function iterate_population(population, target, min_char, max_char)
+    for specimen in population
+        println(fitness(specimen,target))
+    end
+end
+
 function main(target, population_size, min_char, max_char)
     target = string_as_integer_list(target)
+
     target_length = length(target)
     random_population =
-        ranom_population_of_length(population_size, target_length, min_char, max_char)
+        random_population_of_length(population_size, target_length, min_char, max_char)
+
+    iterate_population(random_population, target, min_char, max_char)
 end
 
 min_char, max_char = 32, 122
