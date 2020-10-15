a7dc507e49d5a79c7904d15b2e2d277148a5be61
Eirik Baug <eirikbaug@hotmail.com>
2020-10-15 22:23:17 +0200

    random population

--- main.jl
+++ main.jl
@@ -1,19 +1,23 @@
-min_char, max_char = 32, 122
-
 function string_as_integer_list(string)
 	return [Int(char) for char in string]
 end
 
-function random_specimen_of_length(length, min_char, max_char)
-	return [rand(min_char:max_char) for i = 1:length]
+function ranom_population_of_length(population_size,length, min_char, max_char)
+	return [random_specimen_of_length(length, min_char, max_char) for i in 1:population_size]
 end
 
-function main(target,min_char, max_char)
+function random_specimen_of_length(length, min_char, max_char)
+	return [rand(min_char:max_char) for i in 1:length]
+end
+
+function main(target, population_size, min_char, max_char)
 	target = string_as_integer_list(target)
 	target_length = length(target)
 
-	println(random_specimen_of_length(target_length,min_char, max_char))
-	println(target,target_length)
+	random_population = ranom_population_of_length(population_size,target_length,min_char, max_char)
 end
 
-main("Hello World!",min_char, max_char)
+min_char, max_char = 32, 122
+population_size = 50
+target = "Hello World!"
+main(target,population_size,min_char, max_char)
