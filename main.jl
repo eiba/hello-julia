1edbcdb26c89cc8247bb03b3890ffae68d0aa6fb
Eirik Baug <eirikbaug@hotmail.com>
2020-10-21 00:47:20 +0200

    extracting removal function

--- main.jl
+++ main.jl
@@ -24,6 +24,27 @@ function random_specimen_of_length(target, min_char, max_char)
     return specimen(genotype, fitness(genotype, target))
 end
 
+function rank_sum(ranks)
+    return Int(ranks * (ranks + 1) / 2)
+end
+
+function swap_index_contents(array,i,j)
+    tmp = array[i]
+    array[i] = array[j]
+    array[j] = tmp
+end
+
+function remove(array, index)
+    [
+        array[1:index-1]
+        array[index+1:length(array)]
+    ]
+end
+
+function display_specimen(specimen,iteration)
+    display("$(integer_list_to_string(specimen.genotype)) - {Score: $(specimen.fitness), Iteration: $iteration}")
+end
+
 function fitness(genotype, target)
     fitness = 0
     for i = 1:length(genotype)
@@ -37,21 +58,10 @@ function crossover(genotype1, genotype2)
     return [genotype1[1:split_point]; genotype2[split_point+1:length(genotype2)]]
 end
 
-function rank_sum(ranks)
-    return Int(ranks * (ranks + 1) / 2)
-end
-
-function swap_indexes(array,i,j)
-    tmp = array[i]
-    array[i] = array[j]
-    array[j] = tmp
-end
-
 function select(combined_population, population_size)
     combined_population = sort!(combined_population, by = specimen -> specimen.fitness)
     selected_population = []
-    best_selected_fitness = typemax(Int)
-    best_selected_index = 0
+    best_selected_fitness, best_selected_index = typemax(Int), 0
 
     while length(selected_population) < population_size
         selection_sum = rank_sum(length(combined_population))
@@ -64,19 +74,14 @@ function select(combined_population, population_size)
                 push!(selected_population, individual)
 
                 if  individual.fitness < best_selected_fitness
-                    best_selected_fitness = individual.fitness
-                    best_selected_index = length(selected_population)
+                    best_selected_fitness, best_selected_index = individual.fitness, length(selected_population)
                 end
-
-                combined_population = [
-                    combined_population[1:rank-1]
-                    combined_population[rank+1:length(combined_population)]
-                ]
+                combined_population = remove(combined_population,rank)
                 break
             end
         end
     end
-    swap_indexes(selected_population,1,best_selected_index)
+    swap_index_contents(selected_population,1,best_selected_index)
     return selected_population
 end
 
@@ -90,19 +95,15 @@ function iterate_population(population, target, min_char, max_char)
     return select([population; new_specimen], length(population))
 end
 
-function print_iteration_leader(specimen,iteration)
-    display("$(integer_list_to_string(specimen.genotype)) - {Score: $(specimen.fitness), Iteration: $iteration}")
-end
-
 function main(target, population_size, iterations, min_char, max_char)
     target = string_to_integer_list(target)
     population = random_population_of_length(population_size, target, min_char, max_char)
-    print_iteration_leader(population[1],0)
+    display_specimen(population[1],0)
 
     for i = 1:iterations
         population[1].fitness == 0 ? break :
         population = iterate_population(population, target, min_char, max_char)
-        print_iteration_leader(population[1],i)
+        display_specimen(population[1],i)
     end
 end
 
