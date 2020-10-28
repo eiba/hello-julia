a8d2b36dada6fada75fdf463620a0aec937fd361
Eirik Baug <eirikbaug@hotmail.com>
2020-10-28 21:17:32 +0100

    iris dataset

---
+++ iris.jl
@@ -0,0 +1,14 @@
+using RDatasets
+using StatsPlots
+
+iris = dataset("datasets", "iris");
+@df iris scatter(
+    :SepalLength,
+    :SepalWidth,
+    group = :Species,
+    title = "Iris",
+    xlabel = "Length",
+    ylabel = "Width",
+    m = (0.5, [:cross :hex :star7], 12),
+    bg = RGB(0.2, 0.2, 0.2)
+)
